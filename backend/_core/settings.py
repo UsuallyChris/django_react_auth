a981a69f5b99e905fa300d46281c409f5fa37288
UsuallyChris <schoonc@gmail.com>
2019-05-30 15:42:26 -0700

    URL mapping for login setup

--- backend/_core/settings.py
+++ backend/_core/settings.py
@@ -126,3 +126,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# AUTHENTICATION CONFIG
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated'
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication'
+    ]
+}
---
+++ backend/users/urls.py
@@ -0,0 +1,6 @@
+""" routes for users app """
+from django.urls import path, include
+
+urlpatterns = [
+    path('rest-auth/', include('rest_auth.urls')),
+]
